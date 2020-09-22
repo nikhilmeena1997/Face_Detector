66c61cd5d5f75bee57c6e09e1d6a8245aa142f6a
nikhilmeena1997 <64477950+nikhilmeena1997@users.noreply.github.com>
2020-09-22 13:59:59 +0530

    Face-Detect-On-Testdata

---
+++ new_face_detect.py
@@ -0,0 +1,139 @@
+import numpy as np 
+import cv2
+import os
+
+#KNN algorithm..
+def distance(v1,v2):
+	return np.sqrt(((v1-v2)**2).sum())
+
+
+def knn(train,test,k=5):
+	dist = []
+	
+	for i in range(train.shape[0]):
+
+		ix=train[i, :-1]
+		iy=train[i, -1]
+
+		#computing the distance from test point using euclidean distance...
+		d=distance(test,ix)
+		dist.append([d,iy])
+
+	dk = sorted(dist,key = lambda x: x[0])[:k]
+	
+	#Retrieve only the labels..
+
+	labels = np.array(dk)[:,-1]
+
+	#splitting unique labels with their frequencies..
+
+	output=np.unique(labels,return_counts= True)
+
+	index = np.argmax(output[1])
+
+	return output[0][index]
+
+#####end of KNN..		
+
+cap=cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+skip = 0
+dataset_path = './Face_snaps/'
+
+face_data = []
+labels = []
+
+class_id = 0
+names={}# mapping btw id & name..
+
+#Data Preparation...
+
+for fx in os.listdir(dataset_path):
+	if fx.endswith('.npy'):
+	   
+		#create a mapping btw class_id & name.. 	
+		names[class_id] = fx[:-4]
+		print("Loaded"+fx)
+		data_item = np.load(dataset_path+fx)
+		face_data.append(data_item)
+
+		#creating labels for class..
+		target = class_id*np.ones((data_item.shape[0],))
+
+		class_id+=1
+		labels.append(target)
+face_dataset = np.concatenate(face_data,axis=0)
+face_labels = np.concatenate(labels,axis=0).reshape((-1,1))
+
+print(face_dataset.shape)
+print(face_labels.shape)
+
+trainset = np.concatenate((face_dataset,face_labels),axis=1)
+print(trainset.shape)
+
+#Testing steps..
+
+while True:
+	ret,frame=cap.read()
+
+	if ret==False:
+		continue
+	faces = face_cascade.detectMultiScale(frame,1.3,5)
+	
+	for (x,y,w,h) in faces:
+
+		offset=10
+		face_section = frame[y-offset:y+h+offset,x-offset:x+w+offset]
+		face_section = cv2.resize(face_section,(100,100)) 
+		
+		#predicted label..
+		out = knn(trainset,face_section.flatten())
+		pred_name = names[int(out)]
+		cv2.putText(frame,pred_name,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2,cv2.LINE_AA)
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+	cv2.imshow("Faces",frame)
+
+	key = cv2.waitKey(1) & 0xFF 
+	if key==ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+				
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
